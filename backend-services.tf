b1dd2eba68748c4180b953e2e8ca34e5adee9252
Nurudeen <olaniyi_nuru@aol.com>
2023-02-01 15:10:26 -0600

    bean env

--- backend-services.tf
+++ backend-services.tf
@@ -18,12 +18,12 @@ resource "aws_db_instance" "deen-rds" {
   allocated_storage      = 20
   storage_type           = "gp2"
   engine                 = "mysql"
-  engine_version         = "5.6.34"
+  engine_version         = "8.0.28"
   instance_class         = "db.t2.micro"
   db_name                = var.dbname
   username               = var.dbuser
   password               = var.dbpass
-  parameter_group_name   = "default.mysql5.6"
+  parameter_group_name   = "default.mysql8.0"
   multi_az               = "false"
   publicly_accessible    = "false"
   skip_final_snapshot    = true
@@ -34,9 +34,10 @@ resource "aws_db_instance" "deen-rds" {
 resource "aws_elasticache_cluster" "deen-cache" {
   cluster_id           = "deen-cache"
   engine               = "memcached"
+  engine_version       = "1.6.17"
   node_type            = "cache.t2.micro"
   num_cache_nodes      = 1
-  parameter_group_name = "default.memcached1.5"
+  parameter_group_name = "default.memcached1.6"
   port                 = 11211
   security_group_ids   = [aws_security_group.deen-backend-sg.id]
   subnet_group_name    = aws_elasticache_subnet_group.deen-ecache-subgrp.name
