8eb7e935e4aaa8fd5ab3f32b8fe60f89e407a79b
Nurudeen <olaniyi_nuru@aol.com>
2023-02-01 11:16:13 -0600

    vpc module

--- vpc.tf
+++ vpc.tf
@@ -3,7 +3,7 @@ module "vpc" {
 
   name = var.VPC_NAME
   cidr = var.VpcCIDR
-  azs              = [var.Zone1, var.Zone.2, var.Zone3]
+  azs              = [var.Zone1, var.Zone2, var.Zone3]
   private_subnets  = [var.PrivSub1CIDR, var.PrivSub2CIDR, var.PrivSub3CIDR]
   public_subnets   = [var.PubSub1CIDR, var.PubSub2CIDR, var.PubSub3CIDR]
 
@@ -11,6 +11,7 @@ module "vpc" {
   single_nat_gateway   = true
   enable_dns_hostnames = true
   enable_dns_support   = true
+
 
   tags = {
     Terraform   = "true"
